f70f2f50b7c54887e137d1a0d216e1df4650fd0c
Anusha Vaish <74128712+anusha2402@users.noreply.github.com>
2023-10-23 17:49:57 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (47.70%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-                int mask = 0x55555555;
-        return n > 0 && (n & (n - 1)) == 0 && (n & mask) == n;
+        return n > 0 && (n & (n - 1)) == 0 && n % 3 == 1;
 
     }
 };
